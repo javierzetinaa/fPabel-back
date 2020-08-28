ec54a8a2e151bb45e45e071bf5c599cb4f49f072
ElMariachi <zacag1500@gmail.com>
2020-08-28 17:05:29 -0500

    Segundo Commit

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,10 @@
 process.env.PORT = process.env.PORT || 4000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
-process.env.TOKEN = '48h';
-process.env.SEED = process.env.SEED || 'token-seed-secret-jsjs-quecrazyoso';
+process.env.CADUCIDAD_TOKEN = '48h';
+process.env.SEED = process.env.SEED || 'token-seed-secret-jsjs-quecrazyoso-desarrollo-d';
 let urlDB;
 if (process.env.NODE_ENV === 'dev') urlDB = 'mongodb://localhost:27017/fpanel';
-else urlDB = 'mongodb+srv://fpaneladmin:olaolaola315@fpanel.lejim.mongodb.net/fpanel';
+else urlDB = process.env.MONGO_URI;
 process.env.urlDB = urlDB;
+
+
